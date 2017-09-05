fddc6ed3df52d50431e9d49f26ef5be48a541078
Kevin <klambe17@gmail.com>
2017-09-05 20:04:51 +0100

    wired up endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -22,12 +22,21 @@ def home():
 
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
-
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items':[]
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 @app.route('/store/<string:name>')
 def get_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+
+    return jsonify({'message':'store not found'})
 
 
 @app.route('/store')
@@ -37,12 +46,24 @@ def get_all_stores():
 
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+
+    for store in stores:
+        if store['name'] == name:
+            new_item = request_data['items']
+            store['items'].append(new_item)
+            return jsonify(store)
+
+    return jsonify({'message':'store not found'})
 
 
 @app.route('/store/<string:name>/item')
 def get_items_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store['items'])
+
+    return jsonify({'message':'store not found'})
 
 
 app.run(port=5000)
