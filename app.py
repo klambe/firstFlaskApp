c0e0765d61184e6d7617811abd1175b2aecb6fea
Kevin <klambe17@gmail.com>
2017-09-05 19:29:55 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+stores = [
+    {
+        'name': 'my wonderful store',
+        'items': [
+            {
+                'name': 'my Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+
+@app.route('/')
+def home():
+    return 'home Page not wired up!'
+
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+
+@app.route('/store')
+def get_all_stores():
+    return jsonify({'stores': stores})
+
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
+
+
+app.run(port=5000)
